62acab4d6380d003fb0f78ce5f78665e06b98617
jvo203 <chris.zapart@nao.ac.jp>
2025-12-08 17:19:38 +0900

    gradually getting the code to compile

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+#![recursion_limit = "1024"]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 #[macro_use]
 extern crate ispc;
 
 // Functions exported from ispc will be callable under spmd::*
 ispc_module!(spmd);
+
+use std::{env, ptr};
+use vpx_sys::*;
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 mod fits;
 
@@ -24,4 +34,76 @@ fn main() {
         home_dir
     );
     println!("Test file path: {}", test_file_path);
+}
+
+fn vpx_codec_enc_config_init() -> vpx_codec_enc_cfg_t {
+    vpx_codec_enc_cfg_t {
+        g_usage: 0,
+        g_threads: 0,
+        g_profile: 0,
+        g_w: 0,
+        g_h: 0,
+        g_bit_depth: vpx_bit_depth::VPX_BITS_8,
+        g_input_bit_depth: 8,
+        g_timebase: vpx_rational { num: 0, den: 0 },
+        g_error_resilient: 0,
+        g_pass: vpx_enc_pass::VPX_RC_ONE_PASS,
+        g_lag_in_frames: 0,
+        rc_dropframe_thresh: 0,
+        rc_resize_allowed: 0,
+        rc_scaled_width: 0,
+        rc_scaled_height: 0,
+        rc_resize_up_thresh: 0,
+        rc_resize_down_thresh: 0,
+        rc_end_usage: vpx_rc_mode::VPX_VBR,
+        rc_twopass_stats_in: vpx_fixed_buf {
+            buf: ptr::null_mut(),
+            sz: 0,
+        },
+        rc_firstpass_mb_stats_in: vpx_fixed_buf {
+            buf: ptr::null_mut(),
+            sz: 0,
+        },
+        rc_target_bitrate: 0,
+        rc_min_quantizer: 0,
+        rc_max_quantizer: 0,
+        rc_undershoot_pct: 0,
+        rc_overshoot_pct: 0,
+        rc_buf_sz: 0,
+        rc_buf_initial_sz: 0,
+        rc_buf_optimal_sz: 0,
+        rc_2pass_vbr_bias_pct: 0,
+        rc_2pass_vbr_minsection_pct: 0,
+        rc_2pass_vbr_maxsection_pct: 0,
+        rc_2pass_vbr_corpus_complexity: 0,
+        kf_mode: vpx_kf_mode::VPX_KF_AUTO,
+        kf_min_dist: 0,
+        kf_max_dist: 0,
+        ss_number_layers: 0,
+        ss_enable_auto_alt_ref: [0; 5],
+        ss_target_bitrate: [0; 5],
+        ts_number_layers: 0,
+        ts_target_bitrate: [0; 5],
+        ts_rate_decimator: [0; 5],
+        ts_periodicity: 0,
+        ts_layer_id: [0; 16],
+        layer_target_bitrate: [0; 12],
+        temporal_layering_mode: 0,
+        use_vizier_rc_params: 0,
+        active_wq_factor: vpx_rational { num: 0, den: 0 },
+        err_per_mb_factor: vpx_rational { num: 0, den: 0 },
+        sr_default_decay_limit: vpx_rational { num: 0, den: 0 },
+        sr_diff_factor: vpx_rational { num: 0, den: 0 },
+        kf_err_per_mb_factor: vpx_rational { num: 0, den: 0 },
+        kf_frame_min_boost_factor: vpx_rational { num: 0, den: 0 },
+        kf_frame_max_boost_first_factor: vpx_rational { num: 0, den: 0 },
+        kf_frame_max_boost_subs_factor: vpx_rational { num: 0, den: 0 },
+        kf_max_total_boost_factor: vpx_rational { num: 0, den: 0 },
+        gf_max_total_boost_factor: vpx_rational { num: 0, den: 0 },
+        gf_frame_max_boost_factor: vpx_rational { num: 0, den: 0 },
+        zm_factor: vpx_rational { num: 0, den: 0 },
+        rd_mult_inter_qp_fac: vpx_rational { num: 0, den: 0 },
+        rd_mult_arf_qp_fac: vpx_rational { num: 0, den: 0 },
+        rd_mult_key_qp_fac: vpx_rational { num: 0, den: 0 },
+    }
 }
