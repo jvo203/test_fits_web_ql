dc016713abf142b961fa3a1bfe199fca51d47508
Christopher Zapart <chris.zapart@nao.ac.jp>
2025-12-16 17:03:23 +0900

    stress-testing multithreading

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ use std::ffi::CString;
 use std::fs::File;
 use std::sync::Arc;
 use std::time::Instant;
+use std::time::SystemTime;
 use uuid::Uuid;
 
 use actix::{Actor, Addr};
@@ -110,187 +111,31 @@ async fn main() {
         &server,
     );
 
-    // a x265 video encoding test...
-
-    // set up the dimensions
-    let width = 1024; // a dummy width
-    let height = 768; // a dummy height
-    let depth = fits.depth; // the number of FITS planes (frames)
-
-    println!(
-        "HEVC width: {}, height: {}, depth (number of frames): {}",
-        width, height, depth
-    );
+    let fits = Arc::new(RwLock::new(Box::new(fits)));
+    DATASETS.write().insert(dataid.clone(), fits.clone());
 
     // id: a Vector of String
     let id = vec![dataid.to_string()];
 
-    // create a user session
-    let mut session = UserSession::new(server.clone(), &id);
-    session.width = width;
-    session.height = height;
-    let flux = "logistic".to_string();
-    let target_bitrate = 2000; //kbps
-    let fps = 60;
-    let mut seq_id: i32 = 0;
-    let is_composite = false;
+    //hevc_test(server.clone(), id);
 
-    //HEVC config
-    //alloc HEVC params
-    if session.param.is_null() {
-        session.param = unsafe { x265_param_alloc() };
-        unsafe {
-            //x265_param_default_preset(session.param, CString::new("ultrafast").unwrap().as_ptr(), CString::new("fastdecode").unwrap().as_ptr());
+    let no_threads = 4;
 
-            let tune = CString::new("zerolatency").unwrap();
+    let handles: Vec<std::thread::JoinHandle<()>> = (0..no_threads)
+        .map(|_| {
+            let server_clone = server.clone();
+            let id_clone = id.clone();
+            std::thread::spawn(move || {
+                hevc_test(server_clone, id_clone);
+            })
+        })
+        .collect();
 
-            /*let tune = if fits.telescope.contains("kiso") {
-                CString::new("grain").unwrap()
-            } else {
-                CString::new("zerolatency").unwrap()
-            };*/
-
-            if session.dataset_id.len() == 1 || !is_composite {
-                let preset = CString::new("superfast").unwrap();
-                x265_param_default_preset(session.param, preset.as_ptr(), tune.as_ptr());
-            } else {
-                let preset = CString::new("ultrafast").unwrap();
-                x265_param_default_preset(session.param, preset.as_ptr(), tune.as_ptr());
-            }
-
-            (*session.param).fpsNum = fps as u32;
-            (*session.param).fpsDenom = 1;
-        };
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    unsafe {
-        (*session.param).bRepeatHeaders = 1;
-
-        if session.dataset_id.len() > 1 && is_composite {
-            (*session.param).internalCsp = X265_CSP_I444 as i32;
-        } else {
-            (*session.param).internalCsp = X265_CSP_I400 as i32;
-        }
-
-        (*session.param).internalBitDepth = 8;
-        (*session.param).sourceWidth = width as i32;
-        (*session.param).sourceHeight = height as i32;
-
-        //constant bitrate
-        (*session.param).rc.rateControlMode = X265_RC_METHODS_X265_RC_CRF as i32;
-        (*session.param).rc.bitrate = target_bitrate; //1000;
-    };
-
-    if session.pic.is_null() {
-        session.pic = unsafe { x265_picture_alloc() };
-    }
-
-    if session.enc.is_null() {
-        session.enc = unsafe { x265_encoder_open(session.param) }; //x265_encoder_open_160 for x265 2.8
-        unsafe { x265_picture_init(session.param, session.pic) };
-    }
-
-    //start a video creation event loop
-    session.streaming = true;
-
-    //HEVC (x265) encoding test
-    for frame_idx in 0..depth {
-        println!("Encoding frame {}/{}", frame_idx + 1, depth);
-
-        let watch = Instant::now();
-
-        match fits.get_video_frame(
-            frame_idx,
-            session.width,
-            session.height,
-            &flux,
-            &session.pool,
-        ) {
-            Some(mut y) => {
-                unsafe {
-                    (*session.pic).stride[0] = session.width as i32;
-                    (*session.pic).planes[0] = y.as_mut_ptr() as *mut std::os::raw::c_void;
-                    //adaptive bitrate
-                    (*session.param).rc.bitrate = target_bitrate;
-                }
-
-                let ret = unsafe { x265_encoder_reconfig(session.enc, session.param) };
-
-                if ret < 0 {
-                    println!("x265: error changing the bitrate");
-                }
-
-                let mut nal_count: u32 = 0;
-                let mut p_nal: *mut x265_nal = ptr::null_mut();
-                let p_out: *mut x265_picture = ptr::null_mut();
-
-                //encode
-                let ret = unsafe {
-                    x265_encoder_encode(session.enc, &mut p_nal, &mut nal_count, session.pic, p_out)
-                };
-
-                println!(
-                    "x265 hevc video frame prepare/encode time: {:?}, speed {} frames per second, ret = {}, nal_count = {}",
-                    watch.elapsed(),
-                    1000000000 / watch.elapsed().as_nanos(),
-                    ret,
-                    nal_count
-                );
-
-                //y falls out of scope
-                unsafe {
-                    (*session.pic).stride[0] = 0 as i32;
-                    (*session.pic).planes[0] = ptr::null_mut();
-                }
-
-                //process all NAL units one by one
-                if nal_count > 0 {
-                    let nal_units =
-                        unsafe { std::slice::from_raw_parts(p_nal, nal_count as usize) };
-
-                    for unit in nal_units {
-                        println!("NAL unit type: {}, size: {}", unit.type_, unit.sizeBytes);
-
-                        let payload = unsafe {
-                            std::slice::from_raw_parts(unit.payload, unit.sizeBytes as usize)
-                        };
-
-                        let timestamp = std::time::Instant::now();
-                        seq_id += 1;
-
-                        let ws_frame = WsFrame {
-                            ts: timestamp.elapsed().as_millis() as f32,
-                            seq_id: seq_id as u32,
-                            msg_type: 5, //an hevc video frame
-                            //length: video_frame.len() as u32,
-                            elapsed: watch.elapsed().as_millis() as f32,
-                            frame: payload.to_vec(),
-                        };
-
-                        match encode_to_vec(&ws_frame, config::legacy()) {
-                            Ok(bin) => {
-                                println!("WsFrame binary length: {}", bin.len());
-                                //println!("{}", bin);
-                                //ctx.binary(bin);
-                            }
-                            Err(err) => println!(
-                                "error serializing a WebSocket video frame response: {}",
-                                err
-                            ),
-                        }
-
-                        /*match session.hevc {
-                            Ok(ref mut file) => {
-                                let _ = file.write_all(payload);
-                            }
-                            Err(_) => {}
-                        }*/
-                    }
-                }
-            }
-            None => {}
-        }
-    }
+    println!("HEVC test completed.");
 }
 
 fn vpx_codec_enc_config_init() -> vpx_codec_enc_cfg_t {
@@ -496,6 +341,211 @@ impl Drop for UserSession {
             if !self.pic.is_null() {
                 x265_picture_free(self.pic);
             }
+        }
+    }
+}
+
+// create a separate function to stress-test the Rust-x265 interface (pass the cloned server and id)
+#[allow(dead_code)]
+fn hevc_test(server: Addr<server::SessionServer>, id: Vec<String>) {
+    println!("Starting a separate HEVC test thread...");
+
+    // borrow the fits dataset
+    let datasets = DATASETS.read();
+    let fits = match datasets.get(&id[0]) {
+        Some(x) => x,
+        None => {
+            eprintln!("FITS dataset not found: {}", id[0]);
+            return;
+        }
+    };
+
+    let fits = match fits.try_read() {
+        Some(x) => x,
+        None => {
+            eprintln!("FITS dataset is busy: {}", id[0]);
+            return;
+        }
+    };
+
+    {
+        *fits.timestamp.write() = SystemTime::now();
+    }
+
+    // set up the dimensions
+    let width = 1024; // a dummy width
+    let height = 768; // a dummy height
+    let depth = fits.depth; // the number of FITS planes (frames)
+
+    println!(
+        "HEVC width: {}, height: {}, depth (number of frames): {}",
+        width, height, depth
+    );
+
+    // create a user session
+    let mut session = UserSession::new(server, &id);
+    session.width = width;
+    session.height = height;
+    let flux = "logistic".to_string();
+    let target_bitrate = 2000; //kbps
+    let fps = 60;
+    let mut seq_id: i32 = 0;
+    let is_composite = false;
+
+    //HEVC config
+    //alloc HEVC params
+    if session.param.is_null() {
+        session.param = unsafe { x265_param_alloc() };
+        unsafe {
+            //x265_param_default_preset(session.param, CString::new("ultrafast").unwrap().as_ptr(), CString::new("fastdecode").unwrap().as_ptr());
+
+            let tune = CString::new("zerolatency").unwrap();
+
+            /*let tune = if fits.telescope.contains("kiso") {
+                CString::new("grain").unwrap()
+            } else {
+                CString::new("zerolatency").unwrap()
+            };*/
+
+            if session.dataset_id.len() == 1 || !is_composite {
+                let preset = CString::new("superfast").unwrap();
+                x265_param_default_preset(session.param, preset.as_ptr(), tune.as_ptr());
+            } else {
+                let preset = CString::new("ultrafast").unwrap();
+                x265_param_default_preset(session.param, preset.as_ptr(), tune.as_ptr());
+            }
+
+            (*session.param).fpsNum = fps as u32;
+            (*session.param).fpsDenom = 1;
+        };
+    }
+
+    unsafe {
+        (*session.param).bRepeatHeaders = 1;
+
+        if session.dataset_id.len() > 1 && is_composite {
+            (*session.param).internalCsp = X265_CSP_I444 as i32;
+        } else {
+            (*session.param).internalCsp = X265_CSP_I400 as i32;
+        }
+
+        (*session.param).internalBitDepth = 8;
+        (*session.param).sourceWidth = width as i32;
+        (*session.param).sourceHeight = height as i32;
+
+        //constant bitrate
+        (*session.param).rc.rateControlMode = X265_RC_METHODS_X265_RC_CRF as i32;
+        (*session.param).rc.bitrate = target_bitrate; //1000;
+    };
+
+    if session.pic.is_null() {
+        session.pic = unsafe { x265_picture_alloc() };
+    }
+
+    if session.enc.is_null() {
+        session.enc = unsafe { x265_encoder_open(session.param) }; //x265_encoder_open_160 for x265 2.8
+        unsafe { x265_picture_init(session.param, session.pic) };
+    }
+
+    //start a video creation event loop
+    session.streaming = true;
+
+    //HEVC (x265) encoding test
+    for frame_idx in 0..depth {
+        println!("Encoding frame {}/{}", frame_idx + 1, depth);
+
+        let watch = Instant::now();
+
+        match fits.get_video_frame(
+            frame_idx,
+            session.width,
+            session.height,
+            &flux,
+            &session.pool,
+        ) {
+            Some(mut y) => {
+                unsafe {
+                    (*session.pic).stride[0] = session.width as i32;
+                    (*session.pic).planes[0] = y.as_mut_ptr() as *mut std::os::raw::c_void;
+                    //adaptive bitrate
+                    (*session.param).rc.bitrate = target_bitrate;
+                }
+
+                let ret = unsafe { x265_encoder_reconfig(session.enc, session.param) };
+
+                if ret < 0 {
+                    println!("x265: error changing the bitrate");
+                }
+
+                let mut nal_count: u32 = 0;
+                let mut p_nal: *mut x265_nal = ptr::null_mut();
+                let p_out: *mut x265_picture = ptr::null_mut();
+
+                //encode
+                let ret = unsafe {
+                    x265_encoder_encode(session.enc, &mut p_nal, &mut nal_count, session.pic, p_out)
+                };
+
+                println!(
+                    "x265 hevc video frame prepare/encode time: {:?}, speed {} frames per second, ret = {}, nal_count = {}",
+                    watch.elapsed(),
+                    1000000000 / watch.elapsed().as_nanos(),
+                    ret,
+                    nal_count
+                );
+
+                //y falls out of scope
+                unsafe {
+                    (*session.pic).stride[0] = 0 as i32;
+                    (*session.pic).planes[0] = ptr::null_mut();
+                }
+
+                //process all NAL units one by one
+                if nal_count > 0 {
+                    let nal_units =
+                        unsafe { std::slice::from_raw_parts(p_nal, nal_count as usize) };
+
+                    for unit in nal_units {
+                        println!("NAL unit type: {}, size: {}", unit.type_, unit.sizeBytes);
+
+                        let payload = unsafe {
+                            std::slice::from_raw_parts(unit.payload, unit.sizeBytes as usize)
+                        };
+
+                        let timestamp = std::time::Instant::now();
+                        seq_id += 1;
+
+                        let ws_frame = WsFrame {
+                            ts: timestamp.elapsed().as_millis() as f32,
+                            seq_id: seq_id as u32,
+                            msg_type: 5, //an hevc video frame
+                            //length: video_frame.len() as u32,
+                            elapsed: watch.elapsed().as_millis() as f32,
+                            frame: payload.to_vec(),
+                        };
+
+                        match encode_to_vec(&ws_frame, config::legacy()) {
+                            Ok(bin) => {
+                                println!("WsFrame binary length: {}", bin.len());
+                                //println!("{}", bin);
+                                //ctx.binary(bin);
+                            }
+                            Err(err) => println!(
+                                "error serializing a WebSocket video frame response: {}",
+                                err
+                            ),
+                        }
+
+                        /*match session.hevc {
+                            Ok(ref mut file) => {
+                                let _ = file.write_all(payload);
+                            }
+                            Err(_) => {}
+                        }*/
+                    }
+                }
+            }
+            None => {}
         }
     }
 }
